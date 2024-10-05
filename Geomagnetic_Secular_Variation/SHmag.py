0803c08b4c9070254c29309ee37a8e1f83413d00
Alexandre Fournier <fournier@ipgp.fr>
2024-10-05 19:23:07 +0200

    Updated the notebook for the 2024-2025 academic year

--- Geomagnetic_Secular_Variation/SHmag.py
+++ Geomagnetic_Secular_Variation/SHmag.py
@@ -1,57 +1,61 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 25 10:27:43 2016
+Created on originally on Tue Oct 25 10:27:43 2016
 
 @author: lesur
+subsequently modified by: fournier
 
 Given a set of L(L+2) Gauss coefficients i.e. g10, g11, h11, g20, g21, h21,...
-Computes the X (North), Y (East), Z (Down) components of the magnetic field.
+Computes the X (North), Y (East), Z (Down) components of the magnetic field
+for fields of internal and external origins.
 
 Parameters
 ----------
-ll  : number
-    Maximum degree in  SH expension
-np.pi  : number
-    3.14....
-ra  : number
-    Reference radius (6371.2 km)
-
+li  : number
+    Maximum degree in SH expansion for internal fields
+le  : number
+    Maximum degree in SH expansion for external fields
+np.pi  : number (good old pi)
 """
 
 import numpy as np
 
 # Maximum SH degree of expansion
-ll = 8 
+li = 8  # for the internal field
+le = 1  # for the external field 
 # Reference radius
-# ra = 6371.2 # mean radius of Earth in km
-
+#ra = 6371.2 # mean radius of Earth in km
 
 def mk_cnd(tt, pp, rr):
     #
     # tt (theta), pp (phi), rr (radius) .... all float
     #
     #  Computes the elements of the equations of condition for the three
-    #  components of the magnetic field at a point (theta,phi,r)
+    #  components of the magnetic field generated inside and outside the
+    #  sphere of radius ra at a point (theta,phi,r)
     #
     # ATTENTION : dd = 0. if abs(dd) < 1.e-14
     #
-    # CALLED: SHB_X, SHB_Y, SHB_Z
-    # DEFINED:  ll maximum degreee of SH expension
+    # CALLED: SHB_Xi, SHB_Yi, SHB_Zi, SHB_Xe, SHB_Ye, SHB_Ze
+    # DEFINED:  np.pi 3.14159...
     #           ra reference radius
-    #           np.pi 3.14159...
+    #
     try:
-        aax = SHB_X(tt, pp, rr)
-        aay = SHB_Y(tt, pp, rr)
-        aaz = SHB_Z(tt, pp, rr)
+        aax = SHB_Xi(tt, pp, rr)
+        aax = np.hstack((aax, SHB_Xe(tt, pp, rr)))
+        aay = SHB_Yi(tt, pp, rr)
+        aay = np.hstack((aay, SHB_Ye(tt, pp, rr)))
+        aaz = SHB_Zi(tt, pp, rr)
+        aaz = np.hstack((aaz, SHB_Ze(tt, pp, rr)))
         aa = np.vstack((aax, aay, aaz))
         aa[abs(aa) < 1.e-14] = 0.
     #
         return aa
     except Exception as msg:
-        print ('**** ERROR:')
-        print (msg)
+        print('**** ERROR:')
+        print(msg)
     else:
-        print ('mk_cnd: completed')
+        print('mk_cnd: completed')
 
 
 def WW(nd, eps):
@@ -59,7 +63,7 @@ def WW(nd, eps):
     #   nd (number data), eps (variance data)
     #
     #   Computes Data Weight Matrix (Inverse of Ce)
-    #   data units in nT / yr 
+    #   data units in nT (minimum acceptable sigma 1 pT)
     #
     WW = (1./eps) * (1./eps) * np.eye(nd, nd)
     return WW
@@ -73,12 +77,12 @@ def DD(nm, rr, ra=6371.2):
     dd = np.zeros((nm), dtype=float)
     rw = ra/rr
     # im = 0
-    for il in range(1, ll+1, 1):
+    for il in range(1, li+1, 1):
         k = il*il - 1
         dd[k] = float(il+1)*np.power(rw, float(2*il+4))
     #  im != 0
-    for im in range(1, ll+1, 1):
-        for il in range(im, ll+1, 1):
+    for im in range(1, li+1, 1):
+        for il in range(im, li+1, 1):
             k = il*il+2*im-2
             dd[k] = float(il+1)*np.power(rw, float(2*il+4))
             dd[k+1] = float(il+1)*np.power(rw, float(2*il+4))
@@ -86,54 +90,56 @@ def DD(nm, rr, ra=6371.2):
     return dd
 
 
-def SHB_X(tt, pp, rr, ra=6371.2):
+def SHB_Xi(tt, pp, rr, ra=6371.2):
     #
     # tt (theta), pp (phi), rr (radius) .... all float
     #
     #  Computes the elements of the equations of condition for the
-    #  North component of the magnetic field at a point (theta,phi,r)
+    #  North component of the magnetic field generated inside the sphere of
+    #  radius ra at a point (theta,phi,r)
     #
     # CALLED: mk_dlf
-    # DEFINED:  ll maximum degreee of SH expension
+    # DEFINED:  li maximum degreee of SH expansion
     #           ra reference radius
     #           np.pi 3.14159...
     try:
         dtr = np.pi / 180.
-        aax = np.zeros(ll*(ll+2), dtype=float)
+        aax = np.zeros(li*(li+2), dtype=float)
         rw = ra/rr
         # im = 0
         im = 0
-        plm = mk_dlf(im, tt)
-        for il in range(1, ll+1, 1):
+        plm = mk_dlf(im, li, tt)
+        for il in range(1, li+1, 1):
             k = il*il - 1
             aax[k] = plm[il]*np.power(rw, float(il+2))
         #  im != 0
-        for im in range(1, ll+1, 1):
-            plm = mk_dlf(im, tt)
+        for im in range(1, li+1, 1):
+            plm = mk_dlf(im, li, tt)
             dc = np.cos(float(im)*pp*dtr)
             ds = np.sin(float(im)*pp*dtr)
-            for il in range(im, ll+1, 1):
+            for il in range(im, li+1, 1):
                 k = il*il+2*im-2
                 ww = plm[il]*np.power(rw, float(il+2))
                 aax[k] = ww*dc
                 aax[k+1] = ww*ds
         return aax
     except Exception as msg:
-        print ('**** ERROR:SHB_X')
-        print (msg)
+        print('**** ERROR:SHB_Xi')
+        print(msg)
 
 
-def SHB_Y(tt, pp, rr, ra=6371.2):
+def SHB_Yi(tt, pp, rr, ra=6371.2):
     #
     # tt (theta), pp (phi), rr (radius) .... all float
     #
     #  Computes the elements of the equations of condition for the
-    #  East component of the magnetic field at a point (theta,phi,r)
+    #  East component of the magnetic field generated inside the sphere of
+    #  radius ra at a point (theta,phi,r)
     #
     # ATTENTION : for theta=0 then sin(theta) set to 1.e-10
     #
     # CALLED: mk_lf
-    # DEFINED:  ll maximum degreee of SH expension
+    # DEFINED:  li maximum degreee of SH expansion
     #           ra reference radius
     #           np.pi 3.14159...
     try:
@@ -141,71 +147,185 @@ def SHB_Y(tt, pp, rr, ra=6371.2):
         st = np.sin(tt*dtr)
         if st == 0:
             st = 1.e-10
-        aay = np.zeros(ll*(ll+2), dtype=float)
+        aay = np.zeros(li*(li+2), dtype=float)
         rw = ra/rr
         #  im != 0
-        for im in range(1, ll+1, 1):
-            plm = mk_lf(im, tt)
+        for im in range(1, li+1, 1):
+            plm = mk_lf(im, li, tt)
             dc = -float(im)*np.sin(float(im)*pp*dtr)
             ds = float(im)*np.cos(float(im)*pp*dtr)
-            for il in range(im, ll+1, 1):
+            for il in range(im, li+1, 1):
                 k = il*il+2*im-2
                 ww = plm[il]*np.power(rw, float(il+2))/st
                 aay[k] = -ww*dc
                 aay[k+1] = -ww*ds
         return aay
     except Exception as msg:
-        print ('**** ERROR:SHB_Y')
-        print (msg)
+        print('**** ERROR:SHB_Yi')
+        print(msg)
 
 
-def SHB_Z(tt, pp, rr, ra=6371.2):
+def SHB_Zi(tt, pp, rr, ra=6371.2):
     #
     # tt (theta), pp (phi), rr (radius) .... all float
     #
     #  Computes the elements of the equations of condition for the
-    #  Down component of the magnetic field at a point (theta,phi,r)
+    #  Down component of the magnetic field generated inside the sphere of
+    #  radius ra at a point (theta,phi,r)
     #
     # CALLED: mk_lf
-    # DEFINED:  ll maximum degreee of SH expension
+    # DEFINED:  li maximum degreee of SH expension
     #           ra reference radius
     #           np.pi 3.14159...
     try:
         dtr = np.pi / 180.
-        aaz = np.zeros(ll*(ll+2), dtype=float)
+        aaz = np.zeros(li*(li+2), dtype=float)
         rw = ra/rr
         # im = 0
         im = 0
-        plm = mk_lf(im, tt)
-        for il in range(1, ll+1, 1):
+        plm = mk_lf(im, li, tt)
+        for il in range(1, li+1, 1):
             k = il*il - 1
             aaz[k] = -float(il+1)*plm[il]*np.power(rw, float(il+2))
         #  im != 0
-        for im in range(1, ll+1, 1):
-            plm = mk_lf(im, tt)
+        for im in range(1, li+1, 1):
+            plm = mk_lf(im, li, tt)
             dc = np.cos(float(im)*pp*dtr)
             ds = np.sin(float(im)*pp*dtr)
-            for il in range(im, ll+1, 1):
+            for il in range(im, li+1, 1):
                 k = il*il+2*im-2
                 ww = -float(il+1)*plm[il]*np.power(rw, float(il+2))
                 aaz[k] = ww*dc
                 aaz[k+1] = ww*ds
         return aaz
     except Exception as msg:
-        print ('**** ERROR:SHB_Z')
-        print (msg)
+        print('**** ERROR:SHB_Zi')
+        print(msg)
 
 
-def mk_dlf(im, tt):
+def SHB_Xe(tt, pp, rr, ra=6371.2):
     #
-    # im integer, tt float
+    # tt (theta), pp (phi), rr (radius) .... all float
+    #
+    #  Computes the elements of the equations of condition for the
+    #  North component of the magnetic field generated ouside the sphere
+    #  of radius ra, at a point (theta,phi,r)
+    #
+    # CALLED: mk_dlf
+    # DEFINED:  le maximum degreee of SH expension
+    #           ra reference radius
+    #           np.pi 3.14159...
+    try:
+        dtr = np.pi / 180.
+        aax = np.zeros(le*(le+2), dtype=float)
+        rw = rr/ra
+        # im = 0
+        im = 0
+        plm = mk_dlf(im, le, tt)
+        for il in range(1, le+1, 1):
+            k = il*il - 1
+            aax[k] = plm[il]*np.power(rw, float(il-1))
+        #  im != 0
+        for im in range(1, le+1, 1):
+            plm = mk_dlf(im, le, tt)
+            dc = np.cos(float(im)*pp*dtr)
+            ds = np.sin(float(im)*pp*dtr)
+            for il in range(im, le+1, 1):
+                k = il*il+2*im-2
+                ww = plm[il]*np.power(rw, float(il-1))
+                aax[k] = ww*dc
+                aax[k+1] = ww*ds
+        return aax
+    except Exception as msg:
+        print('**** ERROR:SHB_Xe')
+        print(msg)
+
+
+def SHB_Ye(tt, pp, rr, ra=6371.2):
+    #
+    # tt (theta), pp (phi), rr (radius) .... all float
+    #
+    #  Computes the elements of the equations of condition for the
+    #  East component of the magnetic field generated ouside the sphere
+    #  of radius ra, at a point (theta,phi,r)
+    #
+    # ATTENTION : for theta=0 then sin(theta) set to 1.e-10
+    #
+    # CALLED: mk_lf
+    # DEFINED:  le maximum degreee of SH expension
+    #           ra reference radius
+    #           np.pi 3.14159...
+    try:
+        dtr = np.pi / 180.
+        st = np.sin(tt*dtr)
+        if st == 0:
+            st = 1.e-10
+        aay = np.zeros(le*(le+2), dtype=float)
+        rw = rr/ra
+        #  im != 0
+        for im in range(1, le+1, 1):
+            plm = mk_lf(im, le, tt)
+            dc = -float(im)*np.sin(float(im)*pp*dtr)
+            ds = float(im)*np.cos(float(im)*pp*dtr)
+            for il in range(im, le+1, 1):
+                k = il*il+2*im-2
+                ww = plm[il]*np.power(rw, float(il-1))/st
+                aay[k] = -ww*dc
+                aay[k+1] = -ww*ds
+        return aay
+    except Exception as msg:
+        print('**** ERROR:SHB_Ye')
+        print(msg)
+
+
+def SHB_Ze(tt, pp, rr, ra=6371.2):
+    #
+    # tt (theta), pp (phi), rr (radius) .... all float
+    #
+    #  Computes the elements of the equations of condition for the
+    #  Down component of the magnetic field generated ouside the sphere
+    #  of radius ra, at a point (theta,phi,r)
+    #
+    # CALLED: mk_lf
+    # DEFINED:  le maximum degreee of SH expension
+    #           ra reference radius
+    #           np.pi 3.14159...
+    try:
+        dtr = np.pi / 180.
+        aaz = np.zeros(le*(le+2), dtype=float)
+        rw = rr/ra
+        # im = 0
+        im = 0
+        plm = mk_lf(im, le, tt)
+        for il in range(1, le+1, 1):
+            k = il*il - 1
+            aaz[k] = float(il)*plm[il]*np.power(rw, float(il-1))
+        #  im != 0
+        for im in range(1, le+1, 1):
+            plm = mk_lf(im, le, tt)
+            dc = np.cos(float(im)*pp*dtr)
+            ds = np.sin(float(im)*pp*dtr)
+            for il in range(im, le+1, 1):
+                k = il*il+2*im-2
+                ww = float(il)*plm[il]*np.power(rw, float(il-1))
+                aaz[k] = ww*dc
+                aaz[k+1] = ww*ds
+        return aaz
+    except Exception as msg:
+        print('**** ERROR:SHB_Ze')
+        print(msg)
+
+
+def mk_dlf(im, ll, tt):
+    #
+    # im integer, tt float, ll maximum degreee of SH expension
     #
     # Computes the derivative along theta of all legendre functions for
     # a given order up to degree ll at a given position : theta (in degree)
     #
     # CALLED: mk_lf
-    # DEFINED:  ll maximum degreee of SH expension
-    #           np.pi 3.14159...
+    # DEFINED:  np.pi 3.14159...
+    #
     dlf = np.zeros(ll+1, dtype=float)
     #
     dtr = np.pi / 180.
@@ -220,7 +340,7 @@ def mk_dlf(im, tt):
                 d2 = np.sqrt(float(ii*(ii-2))/float(ii*ii-1))
                 dlf[ii] = d1*dlf[ii-1]-d2*dlf[ii-2]
     else:
-        dlf = mk_lf(im, tt)
+        dlf = mk_lf(im, ll, tt)
         for ii in range(ll, im, -1):
             d1 = np.sqrt(float((ii-im)*(ii+im)))
             d2 = float(ii)
@@ -230,17 +350,17 @@ def mk_dlf(im, tt):
     return dlf
 
 
-def mk_lf(im, tt):
+def mk_lf(im, ll, tt):
     #
-    # im integer, tt float
+    # im integer, tt float, ll maximum degreee of SH expension
     #
     # Computes all legendre functions for a given order up to degree ll
     # at a given position : theta (in degree)
     # recurrence 3.7.28 Fundations of geomagetism, Backus 1996
     #
     # CALLED: logfac
-    # DEFINED:  ll maximum degreee of SH expension
-    #           np.pi 3.14159...
+    # DEFINED:  np.pi 3.14159...
+    #
     lf = np.zeros(ll+1, dtype=float)
     #
     dtr = np.pi / 180.
@@ -285,6 +405,6 @@ def logfac(dd):
     # Calculate log(dd!)
     # dd is a float, but is truncated to an integer before calculation
     #
-    id = np.int(dd)
+    id = int(dd)
     lgfc = np.sum(np.log(range(1, id+1, 1)))
     return lgfc
--- Geomagnetic_Secular_Variation/sv_plots_cartopy.py
+++ Geomagnetic_Secular_Variation/sv_plots_cartopy.py
@@ -10,9 +10,10 @@ def init_conf_plt():
     plt.rc('axes', labelsize=20,facecolor='None')
     plt.rc('savefig', facecolor='None', edgecolor='None')
 
-def mollweide_plot(SVZ, nlat, nlon, colat, longi, Title=None, obs_coords=None, central_long=None):
-    print(np.shape(obs_coords))
-    #init_conf_plt()
+def hammer_plot(SVZ, nlat, nlon, colat, longi, Title=None, obs_coords=None, central_long=None, filename=None, debug=False, unit=None):
+    if debug is True: 
+        print(np.shape(obs_coords))
+    init_conf_plt()
     latit = 90 - colat
     lons, lats = np.meshgrid(longi,latit)
     svz2d =  np.reshape(SVZ, (nlat,nlon) )
@@ -21,11 +22,11 @@ def mollweide_plot(SVZ, nlat, nlon, colat, longi, Title=None, obs_coords=None, c
     fig = plt.figure()#figsize=(10,10))
     if central_long is None:
         central_long = 180
-    ax = plt.axes( projection = ccrs.Mollweide(central_longitude=central_long) )
+    ax = plt.axes( projection = ccrs.Hammer(central_longitude=central_long) )
     ax.set_global()
     ax.coastlines()
     m = ax.contourf(lons, lats, svz2d, levels=clevs, transform = ccrs.PlateCarree(), cmap="RdBu_r", extend='both')
-    c = ax.contour(lons, lats,  svz2d, clevs, colors='k', transform = ccrs.PlateCarree())
+    c = ax.contour(lons, lats,  svz2d, clevs, colors='k', linewidths=0.5,  transform = ccrs.PlateCarree())
     if obs_coords is not None:
         ax.scatter( obs_coords[0,:], obs_coords[1,:], marker="+", color='k', transform = ccrs.PlateCarree())
     cax = fig.add_axes([0.05, 0.08, .9, 0.05])
@@ -33,18 +34,20 @@ def mollweide_plot(SVZ, nlat, nlon, colat, longi, Title=None, obs_coords=None, c
     cax.xaxis.set_label_position('bottom')
     cax.xaxis.set_ticks_position('top')
     cax.xaxis.set_tick_params(pad=0)
-    cm.set_label("nT/yr")
+    if unit is not None:
+        cm.set_label(unit)
+    else: # default unit is SV unit
+        cm.set_label("nT/yr")
     #font_size = 14 # Adjust as appropriate.
     #cm.ax.tick_params(labelsize=font_size)
     cm.ax.tick_params()
     if Title is not None:
         ax.set_title(Title)
 #   
-    #plt.tight_layout()
-    plt.savefig('SVtest.png')
-    plt.savefig('SVtest.pdf')
+    if filename is not None: 
+        plt.savefig(filename + ".png")
+        plt.savefig(filename + ".pdf")
     plt.show()
-
 
 def init_conf_plt_3(): 
 
@@ -52,8 +55,7 @@ def init_conf_plt_3():
    plt.rc('axes', labelsize=14,facecolor='None')
    plt.rc('savefig', facecolor='None', edgecolor='None')
 
-
-def mollweide_plot_3(SVX, SVY, SVZ, nlat, nlon, colat, longi):
+def hammer_plot_3(SVX, SVY, SVZ, nlat, nlon, colat, longi, central_long=None, outfile_name=None):   
 
     init_conf_plt_3()
 
@@ -62,14 +64,16 @@ def mollweide_plot_3(SVX, SVY, SVZ, nlat, nlon, colat, longi):
     lons, lats = np.meshgrid(longi,latit)
     
     gs = gridspec.GridSpec(3, 1)
-    ax1 = plt.subplot(gs[0, 0], projection = ccrs.Mollweide(central_longitude=180) )
-    ax2 = plt.subplot(gs[1, 0], projection = ccrs.Mollweide(central_longitude=180) )
-    ax3 = plt.subplot(gs[2, 0], projection = ccrs.Mollweide(central_longitude=180) )
+    if central_long is None:
+        central_long = 180. 
+    ax1 = plt.subplot(gs[0, 0], projection = ccrs.Hammer(central_longitude=central_long) )
+    ax2 = plt.subplot(gs[1, 0], projection = ccrs.Hammer(central_longitude=central_long) )
+    ax3 = plt.subplot(gs[2, 0], projection = ccrs.Hammer(central_longitude=central_long) )
 
     iplot = 0
     SV = [SVX, SVY, SVZ]
     title = ["dX/dt", "dY/dt", "dZ/dt"]
-    for ax in [ax1, ax2, ax3]:
+    for iplot, ax in enumerate([ax1, ax2, ax3]):
         sv2d =  np.reshape(SV[iplot], (nlat,nlon) )
         maxsv = max(np.abs(SV[iplot]))
         clevs = np.linspace(-maxsv, maxsv, 11, endpoint=True)
@@ -82,29 +86,31 @@ def mollweide_plot_3(SVX, SVY, SVZ, nlat, nlon, colat, longi):
         font_size = 10 # Adjust as appropriate.
         cm.ax.tick_params(labelsize=font_size)
         ax.set_title(title[iplot], fontsize=font_size)
-        iplot = iplot + 1
 #
     plt.tight_layout()
-    plt.savefig('SVtest_3comp.png')
-    plt.savefig('SVtest_3comp.pdf')
+    if outfile_name is not None:
+        plt.savefig(outfile_name+'.png')
+        plt.savefig(outfile_name+'.pdf')
     plt.show()
 
-def plot_residuals(resid, nbins, Title=None, show=True, outfile_name=None):
+def plot_residuals(resid, nbins=20, Title=None, show=True, outfile_name=None, unit=None):
 #   nbins is the number of bins used to represent the histogram
 
     fig = plt.figure()
     fig.subplots_adjust(top=0.98,left=0.08,right=0.9)
     ax1 = fig.add_subplot(111)
     ax1.hist(resid, bins=nbins, rwidth=0.9, edgecolor='None', facecolor='#348ABD')
-    ax1.set_xlabel(r'nT/yr')
+    if unit is not None:
+        ax1.set_xlabel(unit)
+    else:
+        ax1.set_xlabel(r'nT/yr')
     ax1.set_ylabel(r'counts')
     if Title is not None:
         ax1.set_title(Title)
     plt.tight_layout()
-    if outfile_name is None:
-        plt.savefig('myresiduals.pdf')
-    else:
+    if outfile_name is not None:
         plt.savefig(outfile_name+'.pdf')
+        plt.savefig(outfile_name+'.png')
     if show is True:
         plt.show()
 
